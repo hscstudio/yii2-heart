b671015af3fb02e41b31cd9f69ea53bc3f9a370d
Hafid Mukhlasin <milisstudio@gmail.com>
2014-10-13 10:54:16 +0700

    Gii Module

--- modules/gii/module/default/controller.php
+++ modules/gii/module/default/controller.php
@@ -15,7 +15,8 @@ use yii\web\Controller;
 
 class DefaultController extends Controller
 {
-    public function actionIndex()
+    public $layout = '@hscstudio/heart/views/layouts/column2';
+	public function actionIndex()
     {
         return $this->render('index');
     }
